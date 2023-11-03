a4346abf2707c28115a74c4f2d022b154e0de90c
Ishimwe Pacifique <ishimweinstein@gmail.com>
2023-11-03 16:28:35 +0200

    new sign up file

---
+++ signUp.php
@@ -0,0 +1,33 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Sign up</title>
+</head>
+<body>
+    <div>
+        <h1>Please sign Up here!</h1>
+        <div>
+            <form action="">
+                <div class="row">
+                    <label for="firstName">First Name:</label>
+                    <input type="text" name="firstName"> <br>
+                </div>
+                <div class="row"> 
+                    <label for="lastName">Last Name:</label>
+                    <input type="text" name="lastName"> <br>
+                </div>
+                <div class="row">
+                    <label for="email">Email:</label>
+                    <input type="email" name="email"> <br>
+                </div>
+                <div class="row">
+                    <label for="address">Address:</label>
+                    <input type="text" name="address">
+                </div>
+            </form>
+        </div>
+    </div>
+</body>
+</html>
