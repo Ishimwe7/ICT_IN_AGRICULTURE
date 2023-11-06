1a773d607e357321df5d4ab4679369e3b65afbfa
Ishimwe Pacifique <ishimweinstein@gmail.com>
2023-11-06 11:15:16 +0200

    new login file

--- login.php
+++ login.php
@@ -3,9 +3,54 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>login page</title>
+    <title>log in</title>
+    <link rel="stylesheet" href="CSS/signingStyles.css">
 </head>
 <body>
-    
+  <div class="whole-page">
+      <div id="logo">
+          <div class="logo">
+                 <img style="width: 70px;height:50px" src="pictures/logo.jpeg" alt="logo image">
+                </div>
+                <div class="log-text">
+                  <h2>MOD-AGRI</h2>
+              </div>
+      </div>
+      <div class="log-home">
+        <div class="login">
+            <div style="margin-top:50px"></div>
+            <h1 id="heading1">Please Login here!</h1>
+            <div>
+                <form action="" method="post">
+                    <div class="row">
+                        <label for="email">Email:</label>
+                        <input type="email" id="email" required placeholder="Enter your email..." name="email">
+                        <label class="required">*</label>
+                         <br>
+                    </div>
+                    <div class="row">
+                        <label for="email">Password:</label>
+                        <input type="password" id="email" required placeholder="Enter your password here..." name="password">
+                        <label class="required">*</label>
+                         <br>
+                    </div>
+                    <div class="buttons">
+                        <button id="log-in-button" type="submit">Login</button>
+                    </div>
+                </form>
+            </div>
+        </div>
+    <div class="home">
+          <h3 style="font-size: x-large;">Don't have an account? <a href="signUp.php">SignUp here!</a><br><br>
+            
+            Go Back
+            <a href="index.php">home</a>
+          </h3>
+        </div>
+        </div>
+     </div>
+        <script src="JavaScript/signUp.js">
+            //console.log("Hello",document.getElementById("province"));
+        </script>
 </body>
 </html>
