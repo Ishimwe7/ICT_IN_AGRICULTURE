47bb9cd5179b1c109333e4b747fc88246384381c
Ishimwe Pacifique <ishimweinstein@gmail.com>
2023-11-08 17:35:47 +0200

    new file to explore poultry

---
+++ exploreLinks/poultry.php
@@ -0,0 +1,22 @@
+Poultry industry in Rwanda is currently characterized by the coexistence of 2 systems:
+ rudimentary village poultry and industrial poultry at its infancy, the 2 systems facing 
+ scarcity of inputs to fully exploit their potential.
+
+ What kind of chickens are in Rwanda?
+The local breed is called 'Inyarwanda' and is a mixed breed. The exotic layer breeds used 
+are Rhode Island Red, Derco and Norman. 
+The exotic broiler breeds used are Cobb 500 (predominant) and Hubbard.
+
+What is the poultry project in Rwanda?
+The plan aims to transform the poultry industry from a subsistence-based sector to a knowledge-intensive and market-oriented sector, 
+with the goal of raising the number of
+
+What are 5 types of common poultry?
+The term “poultry” covers a wide range of birds, from indigenous and commercial breeds of chickens to Muscovy ducks, mallard ducks, turkeys, guinea fowl, geese, quail, pigeons, ostriches and pheasants. Poultry are raised throughout the world,
+ with chickens by far the leading species everywhere.
+
+ What are the challenges of poultry farming in Rwanda?
+Broiler poultry farmers mentioned many challenges of which lack of quality feeds (59.5 %) and poor access to markets (45.9 %) were most cited. Prevalence of poultry diseases (32.4 %) and lack of training on modern poultry production practices (8.1 %) were also cited as challenges.
+
+
+detailed report on "https://traide.org/blog-2-1/rwanda-poultry-business-opportunity-report"
