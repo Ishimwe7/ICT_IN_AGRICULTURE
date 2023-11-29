10d3e358efffd045e491b5d949f5e5de76e4b39c
Ishimwe Pacifique <ishimweinstein@gmail.com>
2023-11-29 15:55:28 +0200

    added session management codes

--- adminPanel.php
+++ adminPanel.php
@@ -1,3 +1,16 @@
+<?php
+session_start();
+// Check if the session variable is set and get its value
+if (isset($_SESSION['loggedin']) && $_SESSION['loggedin'] === true) {
+    // The user is logged in, retrieve the username or any other session variable
+    $username = $_SESSION['email'];
+    //echo "Welcome, $username!";
+} else {
+    // Redirect to the login page or handle unauthenticated access
+    header("Location: adminLogin.php");
+    exit();
+}
+?>
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -9,7 +22,9 @@
 <body>
         <div id="admi-panel" class="admin-panel">
             <div>
-                <h2 style="text-align: right; color: black">Logged in as:</h2>
+                <h2 style="text-align: right; color: black">Logged in as:
+                <?php echo $username?> 
+            </h2>
             </div>
             <div class="header">
                   <h1>Admin Panel</h1>
@@ -28,7 +43,7 @@
                   </div>
                 </div>
             <div class="logout">
-                <form action="">
+                <form action="logoutUser.php">
                     <button type="submit" name="logout">Logout</button>
                 </form>
             </div>
