9a789da34efed026123d12403f6cb8d7fd82ea8b
Ishimwe Pacifique <ishimweinstein@gmail.com>
2023-11-29 12:26:20 +0200

    display error message to the user

---
+++ error.php
@@ -0,0 +1,60 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Registration Error</title>
+    <style>
+        body {
+            font-family: Arial, sans-serif;
+            background-color: #f8f8f8;
+            text-align: center;
+            margin: 0;
+            padding: 0;
+        }
+
+        .container {
+            max-width: 600px;
+            margin: 50px auto;
+            padding: 20px;
+            background-color: #fff;
+            border-radius: 8px;
+            box-shadow: 0 0 10px rgba(0, 0, 0, 0.1);
+        }
+
+        h1 {
+            color: rgb(128,0,0);
+            font-size: 36px;
+            margin-bottom: 20px;
+        }
+
+        p {
+            color: #333;
+            font-size: 18px;
+            line-height: 1.6;
+            margin-bottom: 30px;
+        }
+
+        .cta-button {
+            display: inline-block;
+            padding: 10px 20px;
+            font-size: 16px;
+            text-decoration: none;
+            color: #fff;
+            background-color: #e74c3c;
+            border-radius: 4px;
+            transition: background-color 0.3s ease;
+        }
+
+        .cta-button:hover {
+            background-color: rgb(128,0,0);
+        }
+    </style>
+</head>
+<body>
+    <div class="container">
+        <h1>Registration Error</h1>
+        <p>Sorry, there was an error processing your registration. Try again later.</p>
+        <a href="signUp.php" class="cta-button">Back to Registration</a>
+    </div>
+</body>
+</html>
